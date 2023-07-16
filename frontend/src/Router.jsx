2c4d153cf880a56ff4b18eac811cd5921ff0f5cd
Mark Bagby <101544990+m-bagby@users.noreply.github.com>
2023-07-16 06:21:45 -0400

    Added a 404 page

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -3,16 +3,18 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import PostsFeed from "./components/post/PostsFeed.jsx";
 import PostPage from "./components/post/PostPage.jsx";
+import Page404 from "./components/Page404";
 
 const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<PostsFeed/>}/>
-        <Route path={"/post/:id"} element={<PostPage/>}/>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<PostsFeed/>}/>
+                <Route path={"/post/:id"} element={<PostPage/>}/>
+                <Route path={"*"} element={<Page404/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default Router;
---
+++ frontend/src/components/Page404.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Page404 = () => {
+    return (
+        <div className={"container postsFeed"}>
+            <div className={"listEnd mTop"}>
+                <h1>404 Page Not Found</h1>
+                <p>Return to <a href={"/"}>Home Page?</a></p>
+            </div>
+        </div>
+    )
+}
+
+export default Page404;
